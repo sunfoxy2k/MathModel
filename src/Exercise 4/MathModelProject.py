d6b07cf7d77b9bf86a8f4c657bf13c95633b6c4c
SivCloud <SivCloud@users.noreply.github.com>
2020-07-15 20:44:30 +0700

    Test new method
    still not get desired R0

--- src/Exercise 4/MathModelProject.py
+++ src/Exercise 4/MathModelProject.py
@@ -3,6 +3,7 @@ import Ex3libMHSimplified as MH
 import csv
 import numpy as np
 import math
+import scipy.special as sp
 
 #calling ex3 sampling function for prior of beta and gamma, the sample is stored in the list 'trace'
 m = int(input("Enter m: "))
@@ -23,6 +24,15 @@ countryList = []
 for i in range (0, numberOfCountry):
     countryList.append(data[0][i+1])
 
+    ##daily case
+confirmedCaseDailyList = [[0 for x in range(numberOfDay)] for y in range(numberOfCountry)]
+for i in range (0, numberOfCountry):
+    for j in range (0, numberOfDay):
+        confirmedCaseDailyList[i][j] = int(data[j+1][i+1])
+        if confirmedCaseDailyList[i][j] == 0: confirmedCaseDailyList[i][j] = 0.0001
+    ##daily case
+
+    ##weekly culmulative
 confirmedCaseList = [[0 for x in range(numberOfWeek)] for y in range(numberOfCountry)] #2d array comprehension?
 for i in range (0, numberOfCountry):
     for j in range (0, numberOfWeek):
@@ -31,6 +41,8 @@ for i in range (0, numberOfCountry):
         for k in range(startDay, startDay + 7):
             sumCaseOfWeek += int(data[k][i+1])
         confirmedCaseList[i][j] = sumCaseOfWeek
+        if confirmedCaseList[i][j] == 0: confirmedCaseList[i][j] = 0.0001
+    ##END weekly culmulative
 
 #i ~ gammaDist(beta, gamma) (gamma is the rate parameter, not scale)
 infectedWeeklyList = [[0 for x in range(numberOfWeek-1)] for y in range(numberOfCountry)]
@@ -40,23 +52,31 @@ for i in range (0, numberOfCountry):
         if infectedWeeklyList[i][j] == 0: infectedWeeklyList[i][j] = 0.0001 
 #csv part done
 #print(infectedWeeklyList)
+#print(confirmedCaseDailyList)
 
 def loglikelihoodXinGammaDist(beta, gamma, X):
     n = len(X)
     return n*np.log(gamma**beta/math.gamma(beta)) + (beta-1)*np.sum(np.log(X)) - gamma*np.sum(X)
 
+gamma_culmulative = lambda a, b, x: sp.gammainc(a, b*x)/sp.gamma(a)
+def loglikelihoodXinCulmulativeGammaDist(beta, gamma, X):
+    sum = 0
+    for xi in X:
+        sum += np.log(gamma_culmulative(beta, gamma, xi))
+    return sum
 
 R0List = []
 for i in range (0, numberOfCountry, 1):
     sum = 0
     for beta, gamma in trace:
-        likelihood = loglikelihoodXinGammaDist(beta, gamma, infectedWeeklyList[i])
-        #if(likelihood < 0): likelihood = 0
+        #likelihood = loglikelihoodXinGammaDist(beta, gamma, infectedWeeklyList[i])
+        likelihood = loglikelihoodXinCulmulativeGammaDist(beta, gamma, confirmedCaseList[i])
+        #likelihood = loglikelihoodXinCulmulativeGammaDist(beta, gamma, confirmedCaseDailyList[i])
         #print(likelihood)
         sum += np.exp(likelihood) * beta/gamma
     R0List.append(sum)
 
 for i in range (0, numberOfCountry, 1):
-    print(countryList[i], ": R0 = ", R0List[i]/m)
+    print(countryList[i], ": R0 = ", R0List[i])
 
 print("Done", flush = True)
