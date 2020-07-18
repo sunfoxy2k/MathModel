c7846af7140974d87ef97627d1c5e79417cd6548
SivCloud <SivCloud@users.noreply.github.com>
2020-07-18 20:07:05 +0700

    Finally we get some concluable R0
    R0 for each country need fine tuning in order to make conclusion

--- src/Exercise 4/Ex4Prior.py
+++ src/Exercise 4/Ex4Prior.py
@@ -2,8 +2,8 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-sigma1 = float(input("Enter sigma1: "))
-sigma2 = float(input("Enter sigma2: "))
+sigma1 = 1.15#float(input("Enter sigma1: "))
+sigma2 = 0.43#float(input("Enter sigma2: "))
 
 normal_density = lambda sigma, mu, x :1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (x - mu)**2 / (2 * sigma**2))
 uniform_density = lambda a, b, x: 1/(b-a) if (a <= x and x <= b) else 0
@@ -13,14 +13,15 @@ def piBeta(beta):
     """
     prior distribution of beta is set to uniform U(10^-10, 1) because of lacking knowlegde for covid-19 tranmission rate 
     """
-    return uniform_density(10**-10, 1, beta)
+    return uniform_density(0.01, 4, beta)
 
 def piGamma(gamma):
     """
     prior distribution of gamma is assumed to follow N(0.5, 0.1), where the mean 0.5 is the inverse of averaged recovery time for
     covid-19 (2 weeks) and the standard deviation 0.1 is estimate for the reported recovered period from ~ 1-4 weeks 
     """
-    return normal_density(0.1, 0.5, gamma)
+    #return normal_density(0.1/7, 0.5/7, gamma)
+    return uniform_density(10**-3, 1, gamma)
 
 def logPDF(t):
     #pi(beta, gamma) = pi(beta) * pi(gamma)
--- src/Exercise 4/MathModelProject.py
+++ src/Exercise 4/MathModelProject.py
@@ -6,73 +6,123 @@ import math
 import scipy.special as sp
 
 #calling ex3 sampling function for prior of beta and gamma, the sample is stored in the list 'trace'
-m = int(input("Enter m: "))
-burnIn = int(input("Enter burnIn: "))
-trace, ac, re, iac, ire = MH.metropolisHasting(m, pr.logPDF, pr.normalProposal, pr.logNormalProposalPDF, [0.5, 2], burnIn)
+m = 5000#int(input("Enter m: "))
+burnIn = 2000#int(input("Enter burnIn: "))
+trace, ac, re, iac, ire = MH.metropolisHasting(m, pr.logPDF, pr.normalProposal, pr.dummyPDF, [1, 0.1], burnIn)
 
 #extracting and reprocess csv, our MH model use week, not day, also, the data is in culmulative form of gamma distribution
-with open('confirmed.csv','rt')as f:
+with open('cumumlativedaily_confirmed.csv','rt')as f:
   fileContent = csv.reader(f)
-  data = []
+  data1 = []
   for row in fileContent:
-      data.append(row)
-numberOfDay = len(data) - 1
+      data1.append(row)
+with open('cumumlativedaily_death.csv','rt')as f:
+  fileContent = csv.reader(f)
+  data2 = []
+  for row in fileContent:
+      data2.append(row)
+with open('cumumlativedaily_recovered.csv','rt')as f:
+  fileContent = csv.reader(f)
+  data3 = []
+  for row in fileContent:
+      data3.append(row)
+
+numberOfDay = len(data1) - 1
 numberOfWeek = int(numberOfDay / 7)
-numberOfCountry = len(data[0]) - 1 #will crash if data is empty
+numberOfCountry = len(data1[0]) - 1 #will crash if data is empty
 
 countryList = []
 for i in range (0, numberOfCountry):
-    countryList.append(data[0][i+1])
+    countryList.append(data1[0][i+1])
 
-    ##daily case
-confirmedCaseDailyList = [[0 for x in range(numberOfDay)] for y in range(numberOfCountry)]
-for i in range (0, numberOfCountry):
-    for j in range (0, numberOfDay):
-        confirmedCaseDailyList[i][j] = int(data[j+1][i+1])
-        if confirmedCaseDailyList[i][j] == 0: confirmedCaseDailyList[i][j] = 0.0001
-    ##daily case
+offset = 0
 
-    ##weekly culmulative
-confirmedCaseList = [[0 for x in range(numberOfWeek)] for y in range(numberOfCountry)] #2d array comprehension?
+IRList = [[0 for x in range(numberOfDay-offset)] for y in range(numberOfCountry)]
 for i in range (0, numberOfCountry):
-    for j in range (0, numberOfWeek):
-        startDay = j*7 + 1
-        sumCaseOfWeek = 0
-        for k in range(startDay, startDay + 7):
-            sumCaseOfWeek += int(data[k][i+1])
-        confirmedCaseList[i][j] = sumCaseOfWeek
-        if confirmedCaseList[i][j] == 0: confirmedCaseList[i][j] = 0.0001
-    ##END weekly culmulative
+    for j in range (0, numberOfDay-offset):
+        IRList[i][j] = int(data1[j+1+offset][i+1])
+IRList = np.array(IRList) 
 
-#i ~ gammaDist(beta, gamma) (gamma is the rate parameter, not scale)
-infectedWeeklyList = [[0 for x in range(numberOfWeek-1)] for y in range(numberOfCountry)]
+RList = [[0 for x in range(numberOfDay-offset)] for y in range(numberOfCountry)]
 for i in range (0, numberOfCountry):
-    for j in range (0, numberOfWeek-1):
-        infectedWeeklyList[i][j] = confirmedCaseList[i][j+1] - confirmedCaseList[i][j]
-        if infectedWeeklyList[i][j] == 0: infectedWeeklyList[i][j] = 0.0001 
+    for j in range (0, numberOfDay-offset):
+        RList[i][j] = int(data2[j+1+offset][i+1]) + int(data3[j+1+offset][i+1])
+RList = np.array(RList)
+
+
+offset += 1        
+IList = [[0 for x in range(numberOfDay-offset)] for y in range(numberOfCountry)]
+for i in range (0, numberOfCountry):
+    for j in range (0, numberOfDay-offset):
+        IList[i][j] = IRList[i][j] - RList[i][j]
+IList = np.array(IList)
+
+dIdRList = [[0 for x in range(numberOfDay-offset)] for y in range(numberOfCountry)]
+for i in range (0, numberOfCountry):
+    for j in range (0, numberOfDay-offset):
+        dIdRList[i][j] = IRList[i][j+1] - IRList[i][j]
+        if dIdRList[i][j] <= 0 : dIdRList[i][j] = 0.0001
+dIdRList = np.array(dIdRList)
+
+dRList = [[0 for x in range(numberOfDay-offset)] for y in range(numberOfCountry)]
+for i in range (0, numberOfCountry):
+    for j in range (0, numberOfDay-offset):
+        dRList[i][j] = RList[i][j+1] - RList[i][j]
+        if dRList[i][j] <= 0 : dRList[i][j] = 0.0001
+dRList = np.array(dRList)
 #csv part done
-#print(infectedWeeklyList)
-#print(confirmedCaseDailyList)
 
-def loglikelihoodXinGammaDist(beta, gamma, X):
+def loglikelihood_standard_normal(X): #for a continuous distribution, this gave a very very low number
     n = len(X)
-    return n*np.log(gamma**beta/math.gamma(beta)) + (beta-1)*np.sum(np.log(X)) - gamma*np.sum(X)
+    return -1/2*np.sum(np.power(X,2))-n*np.log(np.sqrt(2*np.pi))
 
-gamma_culmulative = lambda a, b, x: sp.gammainc(a, b*x)/sp.gamma(a)
-def loglikelihoodXinCulmulativeGammaDist(beta, gamma, X):
-    sum = 0
-    for xi in X:
-        sum += np.log(gamma_culmulative(beta, gamma, xi))
-    return sum
+bound = 0.5 #confidence range, should be positive
+def loglikelihood_standard_normal_accept_ratio(X):
+    return np.log(((X > -bound) & (X < bound )).sum() / len(X))
+
+def loglikelihoodXinGammaDist(beta, gamma, X):#pdf of gammaDist, we don't use this, it's very imprecise
+    n = len(X)
+    return n*beta*np.log(gamma) - n*np.log(sp.gamma(beta)) + (beta-1)*np.sum(np.log(X)) - gamma*np.sum(X)
+
+ContryOffset = [0,0,10,0,0,0,0,0,38,0,0,0,0,0]
+gammaDelay =   [0,0, 0,0,0,7,7,0, 0,0,0,0,0,0]
+betaDelay =    [0,0, 0,0,0,7,7,0, 0,0,0,0,0,0]
 
 R0List = []
+max = -999999
 for i in range (0, numberOfCountry, 1):
+    dRL = []
+    dIdRL = []
+    IListGamma = []
+    IListBeta = []
+    maxDelay = gammaDelay[i] if gammaDelay[i] > betaDelay[i] else betaDelay[i]
+    for j in range(ContryOffset[i],len(dRList[i])-maxDelay):
+        dRL.append(dRList[i][j+gammaDelay[i]])
+        dIdRL.append(dIdRList[i][j+betaDelay[i]])
+        IListGamma.append(IList[i][j])
+        IListBeta.append(IList[i][j])
+    
+    dRL = np.array(dRL)
+    dIdRL = np.array(dIdRL)
+    IListGamma = np.array(IListGamma)
+    IListBeta = np.array(IListBeta)
+
     sum = 0
     for beta, gamma in trace:
-        #likelihood = loglikelihoodXinGammaDist(beta, gamma, infectedWeeklyList[i])
-        likelihood = loglikelihoodXinCulmulativeGammaDist(beta, gamma, confirmedCaseList[i])
-        #likelihood = loglikelihoodXinCulmulativeGammaDist(beta, gamma, confirmedCaseDailyList[i])
-        #print(likelihood)
+        lamdaGamma = (gamma*IListGamma)
+        #likelihoodGamma = loglikelihood_standard_normal((dRL-lamdaGamma) / np.sqrt(lamdaGamma))
+        likelihoodGamma = loglikelihood_standard_normal_accept_ratio((dRL-lamdaGamma) / np.sqrt(lamdaGamma))
+        
+
+        lamdaBeta = (beta*IListBeta)
+        #likelihoodBeta = loglikelihood_standard_normal((dIdRL-lamdaBeta) / np.sqrt(lamdaBeta))
+        likelihoodBeta = loglikelihood_standard_normal_accept_ratio((dIdRL-lamdaBeta) / np.sqrt(lamdaBeta))
+
+        likelihood = likelihoodGamma + likelihoodBeta
+        if likelihood > max:
+            max = likelihood
+            #print("new max: ", likelihood)
+
         sum += np.exp(likelihood) * beta/gamma
     R0List.append(sum)
 
